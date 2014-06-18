60d10257f6096b7de342605634b9fd33d790bd1f
Ben Lubar <ben.lubar@gmail.com>
2014-06-18 12:23:41 -0500

    fix this post: http://forums.thedailywtf.com/forums/p/29620/345284.aspx#345284

--- tdwtf-posts.sql
+++ tdwtf-posts.sql
@@ -8,13 +8,14 @@ SELECT
 	cs_Posts.PostDate AS created_at,
 	'"' + REPLACE(CAST(cs_Posts.Body AS nvarchar(max)), '"', '""') + '"' AS [raw],
 	-- FUCKING HELL why is there no CONCAT function http://stackoverflow.com/a/7806049
-	'"' + REPLACE(STUFF((SELECT ', ' + CAST(cs_Post_Categories.Name AS nvarchar(max))
+	'"' + REPLACE(COALESCE(STUFF((SELECT ', ' + CAST(cs_Post_Categories.Name AS nvarchar(max))
 		FROM cs_Post_Categories
 		WHERE cs_Post_Categories.CategoryID IN (SELECT cs_Posts_InCategories.CategoryID
 			FROM cs_Posts_InCategories
 			WHERE cs_Posts_InCategories.PostID = cs_Posts.PostID)
+		AND cs_Post_Categories.Name NOT LIKE '%' + CHAR(1) + '%'
 		FOR XML PATH(''), TYPE
-		).value('(./text())[1]', 'nvarchar(max)'), 1, 2, ''), '"', '""') + '"' AS tags
+		).value('(./text())[1]', 'nvarchar(max)'), 1, 2, ''), ''), '"', '""') + '"' AS tags
 FROM cs_Posts
 WHERE PostType = 1 AND PostConfiguration = 0
 ORDER BY id ASC;
